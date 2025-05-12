ab04440a6d567fd06b511732e69e9d05333693dd
unknown <Vinculum_IT@Vinculum_IT.vin.net>
2025-05-12 18:33:59 +0530

    second commit

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -1,11 +1,21 @@
-function Cart({ cart }) {
+function Cart({ cart, setallProduct }) {
   console.log("data from the cart comp", cart);
+  function handleIncrement(id) {
+    setallProduct((prevCart) =>
+      prevCart.map((pro) =>
+        pro?.id === id ? { ...pro, count: (pro.count || 0) + 1 } : pro
+      )
+    );
+    
+  }
+  
   return (
     <>
       {cart.length === 0 ? (
         <p>Cart is empty</p>
       ) : (
         cart.map((val, key) => {
+          console.log("count",val?.count)
           return (
             <>
               <div key={key} className="inline-grid m-4">
@@ -28,9 +38,27 @@ function Cart({ cart }) {
                         ₹{val.price}
                       </p>
                     </div>
-                    <button className="mt-3 w-full bg-blue-600 text-white rounded-lg py-2 text-sm hover:bg-blue-700 transition">
+                    <button className="mt-3 w-full bg-blue-600 text-white rounded-lg py-2 text-sm hover:bg-blue-700 transition cursor-pointer">
                       View Details
                     </button>
+                    <div className="flex gap-1">
+                      <button className="px-3 border h-9 mt-5 rounded flex items-center cursor-pointer">
+                        -
+                      </button>
+                      <p className="px-3  h-9 mt-5 rounded flex items-center text-sm">
+                        {val?.count
+                        
+                        }
+                      </p>
+                      <button
+                        onClick={() => {
+                          handleIncrement(val?.id);
+                        }}
+                        className="px-3 py-3 border h-9 mt-5 rounded flex items-center cursor-pointer"
+                      >
+                        +
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
